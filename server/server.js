5b58ac65293c27943503400db291729718dbe1ef
KrunalParmar0 <192807958+KrunalParmar0@users.noreply.github.com>
2026-07-17 17:04:49 +0530

    Cors Updated

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const connectDB = require('./config/db');
 const app = express();
 const employeeRoute = require('./routes/employeeRoute');
 const userRoute = require('./routes/userRoute');
 const studentRoute = require('./routes/studentRoute');
-app.use(cors());
+
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],      
+    credentials: true                                          
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 //DB Connection
 connectDB();
@@ -21,11 +28,12 @@ app.get('/', (req, res) => {
 
 //Routes
 app.use('/employees', employeeRoute);
-app.use('/login',userRoute)
-app.use('/students',studentRoute);
+app.use('/login', userRoute)
+app.use('/students', studentRoute);
 
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT} http://localhost:${PORT}`)});
+    console.log(`Server is running on port ${PORT} http://localhost:${PORT}`)
+});
